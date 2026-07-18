386754020f833ec7681b8c8c62acfa93c94a158f
Sakeena <sakeena2122@gmail.com>
2026-07-18 11:51:42 +0530

    Update API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-  baseURL: "https://personal-finance-backend-uvj8.onrender.com",
+  baseURL: "https://personal-finance-backend-uvj8.onrender.com/api",
 });
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
